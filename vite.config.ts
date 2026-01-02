af758507dbf556b4033f0dc8a7c4cde6564a3f6c
xay <xayderder@gmail.com>
2026-01-02 17:34:38 +0700

    Finalizing The Pastry Chef's Berry layout55555

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Pestry-Berry-Chef/",
   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
+        
       },
     }),
     tailwindcss(),
